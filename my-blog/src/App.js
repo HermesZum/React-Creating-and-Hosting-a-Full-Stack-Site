0a0b6224afca68b2a60027cdcfaf8ac6799e7280
Clemente Nogueira <git.3gear@gmail.com>
2022-11-11 22:11:54 +0000

    The NavBar and its Links were created.

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage";
-import ArticlesListPage from "./pages/ArticlesListPage";
-import ArticlePage from "./pages/ArticlePage";
-import NotFoundPage from "./pages/NotFoundPage";
+import NavBar from './NavBar';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesListPage from './pages/ArticlesListPage';
+import ArticlePage from './pages/ArticlePage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
       <BrowserRouter>
         <div className="App">
-          <h1>My Awesome Blog</h1>
+          <NavBar />
             <div id="page-body">
                 <Routes>
                     <Route path="/" element={<HomePage />} />
