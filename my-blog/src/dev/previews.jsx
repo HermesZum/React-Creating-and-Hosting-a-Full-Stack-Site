eafed6cadb5b3770515890b96e6928dffad1e27a
Clemente Nogueira <git.3gear@gmail.com>
2022-11-12 12:06:41 +0000

    The articles page has been configured.

---
+++ my-blog/src/dev/previews.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox';
+import {PaletteTree} from './palette';
+import App from "../App";
+import ArticlePage from "../pages/ArticlePage";
+
+const ComponentPreviews = () => {
+    return (
+        <Previews palette={<PaletteTree/>}>
+            <ComponentPreview path="/App">
+                <App/>
+            </ComponentPreview>
+            <ComponentPreview path="/ArticlePage">
+                <ArticlePage/>
+            </ComponentPreview>
+        </Previews>
+    );
+};
+
+export default ComponentPreviews;
--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,6 +1,17 @@
+import { useParams } from "react-router-dom";
+import articles from "./article-content";
+
 const ArticlePage = () => {
+    const { articleId } = useParams();
+    const article = articles.find( article => article.name === articleId );
+
     return (
-        <h1>This is the article page!</h1>
+        <>
+            <h1>{ article.title }</h1>
+            { article.content.map( (paragraph, i) => (
+                <p key={i} >{ paragraph }</p>
+            ))}
+        </>
     );
 }
 
