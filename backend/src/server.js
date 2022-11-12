bd2b5aeeae61a2fbe120c00fd1dfb344635fceea
Clemente Nogueira <git.3gear@gmail.com>
2022-11-12 16:29:43 +0000

    The comment system was added and configured in Express.

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,12 +4,15 @@ import express from 'express';
 let articlesInfo = [ {
     name: 'learn-react',
     upvote: 0,
+    comments: [],
 }, {
     name: 'learn-node',
     upvote: 0,
+    comments: [],
 }, {
     name: 'mongodb',
     upvote: 0,
+    comments: [],
 }, ]
 
 /* Creating an instance of the Express application. */
@@ -25,6 +28,20 @@ app.put('/api/articles/:name/upvote', (req, res) => {
     if (article) {
         article.upvote += 1;
         res.send(`The ${ name } article now has ${ article.upvote } up-votes!!!`)
+    }
+    else {
+        res.send('That article doesn\'t exist!')
+    }
+});
+
+/* This is a route handler. It is a function that is called when a request is made to the server. */
+app.post('/api/articles/:name/comment', (req, res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+    const article = articlesInfo.find(a => a.name === name);
+    if (article) {
+        article.comments.push({ postedBy, text });
+        res.send(article.comments)
     }
     else {
         res.send('That article doesn\'t exist!')
