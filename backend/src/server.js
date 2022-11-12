b3b3c0ea69a4d02c3e584f811e5a85b3e5e28e69
Clemente Nogueira <git.3gear@gmail.com>
2022-11-12 15:21:23 +0000

    Express has been configured to use requests as JSON.

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,9 +4,12 @@ import express  from 'express';
 /* Creating an instance of the Express application. */
 const app = express();
 
+/* Telling the server to parse the body of the request as JSON. */
+app.use(express.json());
+
 /* Creating a route that will respond to a GET request to the path /hello with the text "Hello!" */
-app.get('/hello', (req, res) => {
-    res.send('Hello!');
+app.post('/hello', (req, res) => {
+    res.send(`Hello ${req.body.name}!`);
 });
 
 /* Telling the server to listen on port 8000. */
