655fd4cba17dc35c29a698e690c28a046ab3ee2b
Clemente Nogueira <git.3gear@gmail.com>
2022-11-13 21:46:28 +0000

    Added MongoDB database upvote to article page.

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,9 +1,23 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 import NotFoundPage from "./NotFoundPage";
 import articles from "./article-content";
 
 const ArticlePage = () => {
+    const [articleInfo, setArticleInfo] = useState({ upvote: 0, comments: [] });
     const { articleId } = useParams();
+
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${ articleId }`);
+            const newArticleInfo = response.data;
+            setArticleInfo(newArticleInfo);
+        }
+
+        loadArticleInfo().then(r => console.log('The page information was successfully loaded!'));
+    }, []);
+
     const article = articles.find(article => article.name === articleId);
 
     if (!article) {
@@ -13,6 +27,7 @@ const ArticlePage = () => {
     return (
         <>
             <h1>{ article.title }</h1>
+            <p>This article has { articleInfo.upvote } upvote(s)</p>
             { article.content.map((paragraph, i) => (
                 <p key={ i }>{ paragraph }</p>
             )) }
