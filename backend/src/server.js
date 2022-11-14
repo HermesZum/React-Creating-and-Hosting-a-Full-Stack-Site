d52b77b6e785b0226e769d546130d8bf6496610d
Clemente Nogueira <git.3gear@gmail.com>
2022-11-14 21:09:33 +0000

    Protecting the upvote and comment endpoints.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,5 @@
 /* Importing the fs module. */
-import { readFileSync } from "fs";
+import fs from 'fs';
 /* Importing the firebase-admin module. */
 import admin from 'firebase-admin';
 /* Importing the express module. */
@@ -9,13 +9,12 @@ import { connectToDb, db } from "./db.js";
 
 /* Reading the credentials.json file and parsing it into a JavaScript object. */
 const credentials = JSON.parse(
-    readFileSync('../credentials.json', undefined, undefined, null)
+    fs.readFileSync('./credentials.json')
 );
 
 /* Initializing the Firebase Admin SDK. */
 admin.initializeApp({
     credential: admin.credential.cert(credentials),
-
 });
 
 /* Creating an instance of the Express application. */
@@ -27,8 +26,8 @@ app.use(express.json());
 /* A middleware function that is called before any other route handler.
 It checks if the request has an auth_token in the header.
 If it does, it verifies the token and adds the user to the request object. */
-app.use (async (req, res, next) => {
-    const { auth_token } = req.header;
+app.use(async (req, res, next) => {
+    const { auth_token } = req.headers;
     if (auth_token) {
         try {
             req.user = await admin.auth().verifyIdToken(auth_token);
@@ -37,20 +36,20 @@ app.use (async (req, res, next) => {
             res.sendStatus(400);
         }
     }
-
+    req.user = req.user || {};
     next();
 });
 
 /* This is a route handler. It is a function that is called when a request is made to the server. */
 app.get('/api/articles/:name', async(req, res) => {
    const { name } = req.params;
-    const { uid } = req.user;
+   const { uid } = req.user;
 
    const article = await db.collection('articles').findOne({ name });
 
    if (article) {
        const upvoteIds = article.upvoteIds || [];
-       article.canUpvote = uid && !upvoteIds.include(uid);
+       article.canUpvote = uid && !upvoteIds.includes(uid);
        res.json(article);
    }
    else {
@@ -58,18 +57,37 @@ app.get('/api/articles/:name', async(req, res) => {
    }
 });
 
+/* This is a middleware function that is called before any other route handler.
+It checks if the request has an auth_token in the header.
+If it does, it verifies the token and adds the user to the request object. */
+app.use((req, res, next) => {
+    if (req.user) {
+        next();
+    } else {
+        res.sendStatus(401);
+    }
+});
+
 /* This is a route handler. It is a function that is called when a request is made to the server. */
 app.put('/api/articles/:name/upvote', async(req, res) => {
     const { name } = req.params;
-
-    await db.collection('articles').updateOne({ name }, {
-        $inc: { upvote: 1 },
-    });
+    const { uid } = req.user;
 
     const article = await db.collection('articles').findOne({ name });
 
     if (article) {
-        res.json(article)
+        const upvoteIds = article.upvoteIds || [];
+        const canUpvote = uid && !upvoteIds.includes(uid);
+
+        if (canUpvote) {
+            await db.collection('articles').updateOne({ name }, {
+                $inc: { upvote: 1 },
+                $push: { upvoteIds: uid }
+            });
+        }
+
+        const updatedArticle = await db.collection('articles').findOne({ name });
+        res.json(updatedArticle)
     }
     else {
         res.send('That article doesn\'t exist!')
@@ -79,12 +97,12 @@ app.put('/api/articles/:name/upvote', async(req, res) => {
 /* This is a route handler. It is a function that is called when a request is made to the server. */
 app.post('/api/articles/:name/comment', async(req, res) => {
     const { name } = req.params;
-    const { postedBy, text } = req.body;
+    const { text } = req.body;
+    const { email } = req.user;
 
     await db.collection('articles').updateOne({ name }, {
-        $push: { comments: { postedBy, text  } },
+        $push: { comments: { postedBy: email, text  } },
     });
-
     const article = await db.collection('articles').findOne({ name });
 
     if (article) {
