682e51a3fb51b7f49efb35d14207f0bfe71501df
Clemente Nogueira <git.3gear@gmail.com>
2022-11-14 17:05:14 +0000

    Firebase was installed and the authentication system configured.

--- my-blog/src/index.js
+++ my-blog/src/index.js
@@ -5,6 +5,20 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {DevSupport} from "@react-buddy/ide-toolbox";
 import {ComponentPreviews, useInitial} from "./dev";
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDysuT6YTwWZ7MNmV_wgt791Mip4lzOIWA",
+    authDomain: "learning-react-my-blog.firebaseapp.com",
+    projectId: "learning-react-my-blog",
+    storageBucket: "learning-react-my-blog.appspot.com",
+    messagingSenderId: "76737887072",
+    appId: "1:76737887072:web:e040f9e9aa36facb9a3c05"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ my-blog/src/pages/CreateAccountPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CreateAccountPage = () => {
+    return (
+        <h1>Create Account</h1>
+    );
+};
+
+export default CreateAccountPage;
